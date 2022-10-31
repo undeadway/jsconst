6f7857d5eedf418a4c9fbaa43f5c46daffb90013
waygc <hzwaygc@gmail.com>
2022-10-31 12:11:29 +0800

    修改路径

--- modules/Char/Space.js
+++ modules/Char/Space.js
@@ -2,7 +2,6 @@ module.exports = {
     REGX: /\s/, // 标准空白
     SPACE: " ", // 标准空白
 	TAB: "\t",
-    CRLF: "\r\n",
     LF: "\n",
     CR: "\r"
 }
--- modules/Encoding.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    UTF8: "utf-8",
-    ASCII: "ascii",
-	GBK: "GBK",
-	BASE64: "base64",
-    UNICODE: "Unicode"
-}
