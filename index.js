fb918e2816e8f50de291af294a491e1f7c73f0fc
waygc <hzwaygc@gmail.com>
2022-11-10 14:50:32 +0800

    修改优先级

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const fs = require("fs");
-
 const root = {};
 
-function read (modules, parent, object) {
-	modules.map(module => {
-		let path = `${parent}/${module}`;
-		let stat = fs.statSync(path);
-		let name = module.split(".")[0];
-
-		if (stat.isFile()) {
-			if (module === "index.js") return;
-			object[name] = require(path);
-		} else if (stat.isDirectory()) {
-			let index = `${path}/index.js`;
-			let sub = object[name] = fs.existsSync(index) ?  require(index) : {};
-			read(fs.readdirSync(path), path, sub);
-		}
-	});
-}
-
-read(fs.readdirSync(__dirname + "/modules"), __dirname + "/modules", root);
-
 if (typeof(window) !== 'undefined') {
+	
+	const fs = require("fs");
+
+	function read (modules, parent, object) {
+		modules.map(module => {
+			let path = `${parent}/${module}`;
+			let stat = fs.statSync(path);
+			let name = module.split(".")[0];
+	
+			if (stat.isFile()) {
+				if (module === "index.js") return;
+				object[name] = require(path);
+			} else if (stat.isDirectory()) {
+				let index = `${path}/index.js`;
+				let sub = object[name] = fs.existsSync(index) ?  require(index) : {};
+				read(fs.readdirSync(path), path, sub);
+			}
+		});
+	}
+	
+	read(fs.readdirSync(__dirname + "/modules"), __dirname + "/modules", root);
+
 	window.JsConst = root;
 } else {
 	function read (modules, parent, object) {
