60b305142fabdde1136ee52ef845318da2a46d40
waygc <hzwaygc@gmail.com>
2022-11-11 12:17:48 +0800

    更新代码

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ const root = {};
 
 if (typeof(window) !== 'undefined') {
 	
-	const fs = require("fs");
+	function read (now, parent, object) {
 
-	function read (modules, parent, object) {
-		modules.map(module => {
-			let path = `${parent}/${module}`;
-			let stat = fs.statSync(path);
-			let name = module.split(".")[0];
+		const path = `${parent}/${now}/`;
+
+		const _lib = require.context(`${path}.`);
+		_lib.keys().forEach(key => {
+
+			let ret = key.match(/((.+)(\.js)?)/g);
 	
-			if (stat.isFile()) {
-				if (module === "index.js") return;
-				object[name] = require(path);
-			} else if (stat.isDirectory()) {
-				let index = `${path}/index.js`;
-				let sub = object[name] = fs.existsSync(index) ?  require(index) : {};
-				read(fs.readdirSync(path), path, sub);
+			if (ret === null) {
+
+				if (now === "modules") {
+					object[key] = {};
+				} else {
+					object[now] = {};
+				}
+
+				read(key, path, root[key]);
+			} else {
+				object[ret[2]] = _libs(ret[2]);
 			}
 		});
 	}
-	
-	read(fs.readdirSync(__dirname + "/modules"), __dirname + "/modules", root);
+
+	read("modules", ".");
 
 	window.JsConst = root;
 } else {
