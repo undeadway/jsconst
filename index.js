7049940e351505a4d2e8ec662a6d10b0469395a8
waygc <hzwaygc@gmail.com>
2022-11-11 14:53:43 +0800

    修改初始化逻辑

--- index.js
+++ index.js
@@ -2,33 +2,61 @@ const root = {};
 
 if (typeof(window) !== 'undefined') {
 	
-	function read (now, parent, object) {
+	function putObject(parent, names) {
+		let name = names.shift();
+
+		if (!name) return;
+
+		if (!parent[name]) {
+			let object = parent[name] = {};
+			putObject(object, names);
+		}
+	};
+
+	function getObject (parent, names) {
+		let name = names.shift();
+		if (!name) {
+			return parent;
+		} else {
+			if (!parent[name]) { // 这里是为了防止 put 的时候没有 put 进去造成的错误
+				parent[name] = {};
+			}
+			return getObject(parent[name], names);
+		}
+	}
+
+	function read (now, parent) {
 
 		const path = `${parent}/${now}/`;
 
-		const _lib = require.context(`${path}.`);
+		const _lib = require.context("./modules/.");
 		_lib.keys().forEach(key => {
 
-			let ret = key.match(/((.+)(\.js)?)/g);
-	
-			if (ret === null) {
+			let path = key.slice(2);
 
-				let nowObj;
+			/*
+			 * 输出有四种模式：
+			 * 文件夹
+			 * 文件夹名/
+			 * 文件夹/文件名
+			 * 文件夹/文件名.js
+			 * 
+			 * 因为 文件夹名/ 和  文件夹/文件名.js 具有可判断性，所以保留，其余不做处理
+			 */
+			if (path.indexOf(".js") === path.length - 3) {
+				let names = path.slice(0,  path.length - 3).split("/");
+				let fileName = names.pop();
+				let obj = getObject(root, names);
+				obj[fileName] = _lib(key);
+			} else if (path.indexOf("/") === path.length - 1) {
+				let names = path.slice(0,  path.length - 1).split("/");
 
-				if (now === "modules") {
-					nowObj = object[key] = {};
-				} else {
-					nowObj = object[now] = {};
-				}
-
-				read(key, path, nowObj);
-			} else {
-				object[ret[2]] = _libs(ret[2]);
+				putObject(root, names);
 			}
 		});
 	}
 
-	read("modules", ".", root);
+	read("modules", ".");
 
 	window.JsConst = root;
 } else {
